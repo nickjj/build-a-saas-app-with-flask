129fcf234b68bc1bb9341a67f5145004058f18fd
Nick Janetakis <nick.janetakis@gmail.com>
2015-06-04 17:29:19 -0700

    Hardcode logging.INFO for now

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,12 +1,10 @@
 import logging
 import sys
 
-from config import settings
-
 root = logging.getLogger()
-root.setLevel(settings.LOG_LEVEL)
+root.setLevel(logging.INFO)
 
 stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setLevel(settings.LOG_LEVEL)
+stream_handler.setLevel(logging.INFO)
 
 root.addHandler(stream_handler)
