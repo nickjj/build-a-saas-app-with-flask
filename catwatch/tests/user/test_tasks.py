99b69babaf5ed1517ecb0681a4110a8511876241
Nick Janetakis <nick.janetakis@gmail.com>
2015-06-17 12:15:58 -0700

    Remove unnecessary fixture from password reset test

--- catwatch/tests/user/test_tasks.py
+++ catwatch/tests/user/test_tasks.py
@@ -4,7 +4,7 @@ from catwatch.blueprints.user.models import User
 
 
 class TestTasks(object):
-    def test_deliver_password_reset_email(self, session, token):
+    def test_deliver_password_reset_email(self, token):
         """ Deliver a password reset email. """
         with mail.record_messages() as outbox:
             user = User.find_by_identity('admin@localhost.com')
