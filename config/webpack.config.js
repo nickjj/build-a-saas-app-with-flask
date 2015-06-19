869da5188dc0a97b077a80716fced4cf1530a3a7
Nick Janetakis <nick.janetakis@gmail.com>
2015-06-19 07:38:41 -0700

    Optimize and document the webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,32 +1,109 @@
 var fs = require('fs');
 var path = require('path');
 
+// Webpack and third party plugins.
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var AutoPrefixerPlugin = require('autoprefixer-core');
 var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
 
+// Environment detection.
+var node_env = process.env.NODE_ENV || 'development';
+
+/*
+ Configuration settings
+ ------------------------------------------------------------------------------
+ */
+
+// Where is your project located relative to this config?
+var context = path.join(__dirname, '..');
+
+// Where are your source assets located?
 var rootAssetPath = './catwatch/assets';
+var contextRoot = path.join(context, rootAssetPath);
+
+// Where will the files get built to?
+var buildOutputPath = './build/public';
+
+// How should certain asset types be configured?
+var assets = {
+    fonts: {
+        path: 'fonts',
+        filename: '[path][name].[hash].[ext]'
+    },
+    images: {
+        path: 'images',
+        filename: '[path][name].[hash].[ext]'
+    },
+    scripts: {
+        path: 'scripts',
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[id].[chunkhash].js'
+    },
+    styles: {
+        path: 'styles',
+        filename: '[name].[chunkhash].css'
+    }
+};
+
+// Which top level JS and CSS files should get output?
+var chunks = {
+    app_js: [
+        path.join(contextRoot, assets.styles.path, 'main.scss')
+    ],
+    app_css: [
+        path.join(contextRoot, assets.styles.path, 'main.scss'),
+        path.join(contextRoot, assets.styles.path,
+            'vendor', 'font-awesome.4.3.0.css'),
+        path.join(contextRoot, assets.styles.path,
+            'vendor', 'rome.2.1.17.css')
+    ]
+};
+
+// Where will assets get served in development mode? This depends on running
+// the webpack dev server.
+var publicPath = process.env.PUBLIC_PATH || 'http://localhost:2992/assets/';
+
+/*
+ Do not edit past this line unless you are tinkering with webpack.
+ ------------------------------------------------------------------------------
+ */
+
+// Plugins that will load in all environments.
+var plugins = [
+    // http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+    new webpack.NoErrorsPlugin(),
+
+    // https://github.com/webpack/extract-text-webpack-plugin
+    new ExtractTextPlugin(assets.styles.filename),
+
+    // https://github.com/nickjj/manifest-revision-webpack-plugin
+    new ManifestRevisionPlugin(path.join('build', 'manifest.json'), {
+        rootAssetPath: path.join(rootAssetPath, assets.images.path)
+    })
+];
+
+// Development environment only plugins.
+if (node_env !== 'development') {
+    var developmentPlugins = [
+        // http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+        new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}}),
+
+        // http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+        new webpack.optimize.DedupePlugin()
+    ];
+
+    plugins.push(developmentPlugins[0])
+}
 
 module.exports = {
     context: path.join(__dirname, '../'),
-    entry: {
-        app_js: [
-            rootAssetPath + '/scripts/entry.js'
-        ],
-        app_css: [
-            rootAssetPath + '/styles/main.scss',
-            rootAssetPath + '/styles/vendor/font-awesome.4.3.0.css',
-            rootAssetPath + '/styles/vendor/rome.2.1.17.css'
-        ]
-    },
+    entry: chunks,
     output: {
-        // Everything will get built to this directory.
-        path: './build/public',
-        // Assets will be served from this path as its base. This could be
-        // swapped out to be a CDN later.
-        publicPath: 'http://localhost:2992/assets/',
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[id].[chunkhash].js'
+        path: buildOutputPath,
+        publicPath: publicPath,
+        filename: assets.scripts.filename,
+        chunkFilename: assets.scripts.chunkFilename
     },
     resolve: {
         // Allow requiring files without supplying the extension.
@@ -34,57 +111,32 @@ module.exports = {
     },
     module: {
         loaders: [
-            // Add loaders for a wide array of common extensions.
             {
                 test: /\.js$/i, loader: 'babel-loader?stage=0',
                 exclude: /node_modules/
             },
             {
                 test: /\.s?css$/i,
-                // Extract the file name so that we can later output a css
-                // file separately in the plugins section.
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader?includePaths[]=' + 'catwatch/assets')
+                loader: ExtractTextPlugin.extract('style-loader',
+                    'css-loader!postcss-loader!sass-loader?includePaths[]=' + path.join(contextRoot, assets.styles.path))
             },
             {
                 test: /\.(jpe?g|png|gif|svg([\?]?.*))$/i,
                 loaders: [
-                    // Optimize images, the filename will get md5'd by default.
-                    'file?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]',
+                    'file?context=' + rootAssetPath + '&name=' + assets.images.filename,
                     'image?bypassOnDebug&optimizationLevel=7&interlaced=false'
                 ]
             },
-            {test: /\.(woff([\?]?.*)|woff2([\?]?.*))$/i, loader: 'url-loader?limit=100000'},
+            {
+                test: /\.(woff([\?]?.*)|woff2([\?]?.*))$/i,
+                loader: 'url-loader?limit=100000'
+            },
             {
                 test: /\.(ttf([\?]?.*)|eot([\?]?.*))$/i,
-                loader: 'file-loader?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]'
-            },
-            {
-                test: /\.(wav|mp3)$/i,
-                loader: 'file-loader?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]'
-            },
-            {
-                test: /\.(html)$/i,
-                loader: 'file-loader?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]'
-            },
-            {
-                test: /\.(md|markdown)$/i,
-                loader: 'file-loader?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]'
+                loader: 'file-loader?context=' + rootAssetPath + '&name=' + assets.fonts.filename
             }
         ]
     },
-    plugins: [
-        //new webpack.optimize.CommonsChunkPlugin('common',
-        //    'common.[chunkhash].js'),
-        new ExtractTextPlugin('[name].[chunkhash].css'),
-        new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}}),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {NODE_ENV: JSON.stringify('production')}
-        }),
-        new webpack.NoErrorsPlugin(),
-        new ManifestRevisionPlugin(path.join('build', 'manifest.json'), {
-            rootAssetPath: rootAssetPath,
-            ignorePaths: ['/fonts', '/styles', '/scripts']
-        })
-    ]
+    plugins: plugins,
+    postcss: [AutoPrefixerPlugin()]
 };
