d2e370743b0338c601cee00e72a6cf9a4a3ed062
Nick Janetakis <nick.janetakis@gmail.com>
2015-06-23 14:51:59 -0700

    Add random invoices to all fake accounts

--- cli/commands/cmd_add.py
+++ cli/commands/cmd_add.py
@@ -21,12 +21,14 @@ from catwatch.app import create_app
 from catwatch.extensions import db
 from catwatch.blueprints.issue.models import Issue
 from catwatch.blueprints.user.models import User
+from catwatch.blueprints.billing.models.invoice import Invoice
 from catwatch.blueprints.billing.models.coupon import Coupon
 from catwatch.blueprints.billing.gateways.stripecom import \
     Coupon as PaymentCoupon
 
 fake = Faker()
 app = create_app()
+db.app = app
 
 
 def _log_status(count, model_label):
@@ -187,6 +189,58 @@ def coupons():
 
 
 @click.command()
+def invoices():
+    """
+    Create random invoices.
+    """
+    data = []
+
+    users = db.session.query(User).all()
+
+    for user in users:
+        for i in range(0, random.randint(1, 12)):
+            # Create a fake unix timestamp in the future.
+            period_start_on = fake.date_time_between(
+                start_date='now', end_date='+1y').strftime('%s')
+            period_end_on = fake.date_time_between(
+                start_date=period_start_on, end_date='+14d').strftime('%s')
+            exp_date = fake.date_time_between(
+                start_date='now', end_date='+2y').strftime('%s')
+
+            period_start_on = datetime.utcfromtimestamp(
+                float(period_start_on)).strftime('%Y-%m-%d')
+            period_end_on = datetime.utcfromtimestamp(
+                float(period_end_on)).strftime('%Y-%m-%d')
+            exp_date = datetime.utcfromtimestamp(
+                float(exp_date)).strftime('%Y-%m-%d')
+
+            plans = ['BRONZE', 'GOLD', 'PLATINUM']
+            cards = ['Visa', 'Mastercard', 'AMEX',
+                     'J.C.B', "Diner's Club"]
+
+            params = {
+                'created_on': fake.date_time_this_year(),
+                'updated_on': fake.date_time_this_year(),
+                'user_id': user.id,
+                'receipt_number': fake.md5(),
+                'description': '{0} MONTHLY'.format(random.choice(plans)),
+                'period_start_on': period_start_on,
+                'period_end_on': period_end_on,
+                'currency': 'usd',
+                'tax': random.random() * 100,
+                'tax_percent': random.random() * 10,
+                'total': random.random() * 1000,
+                'brand': random.choice(cards),
+                'last4': random.randint(1000, 9000),
+                'exp_date': exp_date
+            }
+
+            data.append(params)
+
+    return _bulk_insert(Invoice, data, 'invoices')
+
+
+@click.command()
 @click.pass_context
 def all(ctx):
     """
@@ -198,6 +252,7 @@ def all(ctx):
     ctx.invoke(users)
     ctx.invoke(issues)
     ctx.invoke(coupons)
+    ctx.invoke(invoices)
 
     return None
 
@@ -205,4 +260,5 @@ def all(ctx):
 cli.add_command(users)
 cli.add_command(issues)
 cli.add_command(coupons)
+cli.add_command(invoices)
 cli.add_command(all)
