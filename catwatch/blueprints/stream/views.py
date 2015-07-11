7e5b3e63ba0d3ad23ae67f2177de3ced39184dd9
Nick Janetakis <nick.janetakis@gmail.com>
2015-07-11 17:04:57 -0400

    Prevent unsubscribed users from seeing the stream

--- catwatch/blueprints/stream/views.py
+++ catwatch/blueprints/stream/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, current_app, render_template
-from flask_login import login_required
+from flask import Blueprint, current_app, render_template, redirect, url_for
+from flask_login import login_required, current_user
 
 stream = Blueprint('stream', __name__, template_folder='templates')
 
@@ -7,6 +7,9 @@ stream = Blueprint('stream', __name__, template_folder='templates')
 @stream.route('/live_stream')
 @login_required
 def live_stream():
+    if not current_user.subscription:
+        return redirect(url_for('billing.pricing'))
+
     faye = {
         'public_url': current_app.config.get('BROADCAST_PUBLIC_URL')
     }
