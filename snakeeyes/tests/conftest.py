945e8a256e384ed441fc8f9ec4cf15743ecf5b22
Nick Janetakis <nick.janetakis@gmail.com>
2021-03-06 08:30:56 -0500

    Fix deprecration warnings for pytest

--- snakeeyes/tests/conftest.py
+++ snakeeyes/tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from snakeeyes.app import create_app
 
 
-@pytest.yield_fixture(scope='session')
+@pytest.fixture(scope='session')
 def app():
     """
     Setup our flask test app, this only gets executed once.
@@ -27,7 +27,7 @@ def app():
     ctx.pop()
 
 
-@pytest.yield_fixture(scope='function')
+@pytest.fixture(scope='function')
 def client(app):
     """
     Setup an app client, this gets executed for each test function.
