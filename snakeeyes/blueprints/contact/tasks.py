fe8266beb5541f1ac5b08f365edb5f30b3e8eddd
Nick Janetakis <nick.janetakis@gmail.com>
2021-03-22 11:08:57 -0400

    Fix contact form recipient email address
    Since Celery is configured differently now we can't reach in and
    grab config values outside of Celery.

--- snakeeyes/blueprints/contact/tasks.py
+++ snakeeyes/blueprints/contact/tasks.py
@@ -1,3 +1,5 @@
+from flask import current_app
+
 from lib.flask_mailplus import send_template_message
 from snakeeyes.app import create_celery_app
 
@@ -19,7 +21,7 @@ def deliver_contact_email(email, message):
 
     send_template_message(subject='[Snake Eyes] Contact',
                           sender=email,
-                          recipients=[celery.conf.get('MAIL_USERNAME')],
+                          recipients=[current_app.config['MAIL_USERNAME']],
                           reply_to=email,
                           template='contact/mail/index', ctx=ctx)
 
