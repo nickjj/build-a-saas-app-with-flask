db1f0a09f5620bd2da188b261391e02d2d88b500
Nick Janetakis <nick.janetakis@gmail.com>
2021-05-13 13:43:01 -0400

    Use new Flask 2.0 .get() decorator function

--- snakeeyes/blueprints/page/views.py
+++ snakeeyes/blueprints/page/views.py
@@ -6,22 +6,22 @@ from snakeeyes.extensions import redis
 page = Blueprint('page', __name__, template_folder='templates')
 
 
-@page.route('/')
+@page.get('/')
 def home():
     return render_template('page/home.html')
 
 
-@page.route('/terms')
+@page.get('/terms')
 def terms():
     return render_template('page/terms.html')
 
 
-@page.route('/privacy')
+@page.get('/privacy')
 def privacy():
     return render_template('page/privacy.html')
 
 
-@page.route('/up')
+@page.get('/up')
 def up():
     redis.ping()
     return ''
