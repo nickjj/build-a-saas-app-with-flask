7d9bfd9fea26efd29d4fdabd3c9f999c46d1dc45
Nick Janetakis <nick.janetakis@gmail.com>
2022-10-30 15:59:50 -0400

    Log Gunicorn response time in ms instead of μs

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -8,7 +8,7 @@ from distutils.util import strtobool
 
 bind = os.getenv('WEB_BIND', '0.0.0.0:8000')
 accesslog = '-'
-access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
+access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(M)sms"  # noqa: E501
 
 workers = int(os.getenv('WEB_CONCURRENCY', multiprocessing.cpu_count() * 2))
 threads = int(os.getenv('PYTHON_MAX_THREADS', 1))
